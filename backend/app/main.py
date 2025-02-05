00c1e3d2db1af5f4df0e412ac8afad4a25bfeaaf
yoonjinsoo <psyjs1@gmail.com>
2025-02-05 21:20:36 +0900

    main.py 파일에 추가

---
+++ backend/app/main.py
@@ -0,0 +1,11 @@
+﻿from fastapi import FastAPI
+from app.api.v1.endpoints import content
+
+app = FastAPI()
+
+# API 라우터 등록
+app.include_router(content.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the CEO Blog Editor API!"} 
